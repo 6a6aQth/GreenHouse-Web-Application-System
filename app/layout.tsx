1b8ce419cb7e526d416986c8d5c820b3c298f977
6a6aQth <qthmichaels@gmail.com>
2025-09-28 18:36:40 +0200

    Meta Data

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Smart Agri Solutions',
-  description: 'Created with v0',
+  description: 'Created by MidasCreed',
   generator: 'v0.dev',
 }
 
